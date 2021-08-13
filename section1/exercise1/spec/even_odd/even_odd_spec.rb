1c45cf695d6ab044a44fc94264a217dd6e492923
rafaelpvs <rafael12wq@gmail.com>
2021-08-13 11:30:12 -0300

    add specify and example

--- section1/exercise1/spec/even_odd/even_odd_spec.rb
+++ section1/exercise1/spec/even_odd/even_odd_spec.rb
@@ -11,12 +11,12 @@ describe ArrayOfNumbers do
         has_even = even_odd.has_even([1, 3, 5, 7, 9])
         expect(has_even).to eq(false)
     end
-    it 'use has_odd for an array of numbers' do
+    example 'use has_odd for an array of numbers' do
         even_odd = ArrayOfNumbers.new
         has_odd = even_odd.has_odd([1, 1, 2, 3, 4])
         expect(has_odd).to eq(true)
     end
-    it 'use has_odd for an array of numbers' do
+    specify 'use has_odd for an array of numbers' do
         even_odd = ArrayOfNumbers.new
         has_odd = even_odd.has_odd([2, 4, 6, 8, 10])
         expect(has_odd).to eq(false)
