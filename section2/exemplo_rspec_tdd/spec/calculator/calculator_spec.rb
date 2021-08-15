5a8ba1c9f23af17c5b12606c00081955cdb6a622
Rafael Pinto <rafael12wq@gmail.com>
2021-08-15 20:58:22 -0300

    make the test pending

--- section2/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ section2/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -7,10 +7,12 @@ describe ' Class Calculator' do
             #result = subject.sum(5, 7) --> the class can be referenced directly by the subject
             expect(result).to eq(12)
         end
-        it 'with negative positive numbers' do 
+        xit 'with negative positive numbers' do # xit makes the test pending
             result = calc.sum(-5, 7)
             expect(result).to eq(2)
         end
+
+        it 'pendding'  # when we don't complete, we also flag with pending
 
         it 'with negative numbers' do 
             result = calc.sum(-5, -7)
