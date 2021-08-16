8827ce3ee75fa2417499904f55958b49a1fadce2
Rafael Pinto <rafael12wq@gmail.com>
2021-08-15 21:42:00 -0300

    add comment with class name on describe

--- section2/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ section2/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,10 +1,11 @@
 require 'calculator'
-describe ' Class Calculator' do 
+describe Calculator, 'About the calculator' do 
     context '#sum' do
-        subject(:calc) { Calculator.new() }
+        subject(:calc) { Calculator.new() } # explicit subject. we can pass arguments on new method.
+                                            # if the context conteins the class instead of a string we can use described_class.new
         it 'with positive numbers' do 
             result = calc.sum(5, 7)
-            #result = subject.sum(5, 7) --> the class can be referenced directly by the subject
+            #result = subject.sum(5, 7) --> the class can be referenced directly by the subject -> implicit subject
             expect(result).to eq(12)
         end
         xit 'with negative positive numbers' do # xit makes the test pending
